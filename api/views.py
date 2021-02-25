d5c65eebce40d465c7af7358fea94d0b20e4171a
asadA009 <asadali4345425@gmail.com>
2021-02-25 15:12:26 +0500

    second

--- api/views.py
+++ api/views.py
@@ -4,10 +4,14 @@ from .models import student
 from django.http import HttpResponse
 from .serializers import studentSerializers
 from rest_framework.renderers import JSONRenderer
-def student_detail(request):
-    stu=student.objects.all()
-    serializer=studentSerializers(stu,many=True)
-    json_data=JSONRenderer().render(serializer.data)
-    return HttpResponse(json_data, content_type='application/json')
+from rest_framework import viewsets
+
+class studenteView(viewsets.ModelViewSet):
+    queryset=student.objects.all()
+    serializer_class= studentSerializers
+#def student_detail(request):
+ ##  serializer=studentSerializers(stu,many=True)
+   # json_data=JSONRenderer().render(serializer.data)
+    #return HttpResponse(json_data, content_type='application/json')
 
     
--- restapi/urls.py
+++ restapi/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from api import views
+from rest_framework import routers
+from rest_framework import serializers
+router=routers.DefaultRouter()
+router.register('stuinfo',views.studenteView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stuinfo/', views.student_detail),
+    path('', include(router.urls)),
 ]
